cd88dbdbbc3af78527f9c40ce6ee0720c0d6a8cc
pyj1110 <xjzl98@naver.com>
2025-08-29 12:34:09 +0900

    20250829

---
+++ game/__init__.py
@@ -0,0 +1,12 @@
+# game/__init__.py
+from .graphic.render import render_test
+from .sound.echo import echo_test
+
+VERSION = 3.5
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+
+# 패키지 초기화 코드 작성
+# 초기화 코드는 한 번 실행된 후에는 다시 import해도 실행되지 않음!!
+print("initialzing game...")
---
+++ sys1.py
@@ -0,0 +1,5 @@
+import sys
+
+arges = sys.argv[1:]
+for i in arges:
+    print(i)
